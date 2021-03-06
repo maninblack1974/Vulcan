ad030f0352ac1d9daa948e6006b28e02f3c5bb30
Tim Hoyt <timhoyt824@gmail.com>
2021-03-06 13:47:58 -0800

    working on mt sql connection

--- customerInfo.sql
+++ customerInfo.sql
@@ -1,15 +1,31 @@
-Create DATABASE customers_db ;
+Create DATABASE vulcan_db ;
 
 CREATE TABLE customer_info
 (
 	id int NOT NULL AUTO_INCREMENT,
-	customer_name VARCHAR(255) NOT NULL,
-    company_name VARCHAR(255),
-    address VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NUlL,
-    user_password VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL,
+	customer_firstName VARCHAR(255) NOT NULL,
+    customer_lastName VARCHAR(255) NOT NULL,
+    customer_companyName VARCHAR(255),
+    customer_address VARCHAR(255) NOT NULL,
+    customer_email VARCHAR(255) NOT NUlL,
+    customer_userPassword VARCHAR(255) NOT NULL,
+    customer_url VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE servicePro_info
+(
+id int NOT NULL AUTO_INCREMENT,
+	servicePro_firstName VARCHAR(255) NOT NULL,
+    servicePro_lastName VARCHAR(255) NOT NULL,
+    servicePro_companyName VARCHAR(255),
+    servicePro_address VARCHAR(255) NOT NULL,
+    servicePro_email VARCHAR(255) NOT NUlL,
+    servicePro_userPassword VARCHAR(255) NOT NULL,
+    servicePro_url VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 INSERT INTO customer_info () VALUES ();
+
+INSERT INTO servicePro_info () VALUES ();
