c6d1f621d818d6dff97593908c33a5cddf66fe6f
Tim Hoyt <timhoyt824@gmail.com>
2021-03-13 10:28:40 -0800

    seed data

---
+++ db_seed/seed.sql
@@ -0,0 +1,17 @@
+use vulcan_db;
+
+INSERT INTO servicePros
+    (servicePro_firstName, servicePro_lastName, servicePro_email, servicePro_userPassword, servicePro_companyName, servicePro_url, servicePro_category, servicePro_address, servicePro_city, servicePro_state, servicePro_zipCode)
+VALUES
+    ("Bob", "Knight", "bob@test.com", "123456", "Bobs Plumbing", "www.bobsplumbing.com", "Plumber", "3310 Walnut Ln", "Random City", "CA", "92101"),
+    ("John", "Tran", "john@test.com", "654321", "Johns Electrical", "www.johnselectrical.com", "Electrican", "807 Elm St", "Random City2", "CA", "92019"),
+    ("Sarah", "Rodriguez", "sarah@test.com", "321456", "Sarahs HVAC", "www.sarahhvac.com", "HVAC", "908 Main St", "Random City3", "CA", "92015"),
+    ("Diego", "Gomez", "diego@test.com", "123654", "Diego Refrigeration", "www.diegofrig.com", "Refrigeration", "398 Ocean Blvd", "Random City4", "CA", "92101"),
+    ("Kumal", "Singh", "kumal@test.com", "456123", "Kumals Cleaning", "www.kumalscleaning.com", "General Cleaning", "668 Juniper St", "Random City5", "CA", "92103"),
+    ("Megan", "Bond", "megan@test.com", "321654", "Megans Linens", "www.meganslinens.com", "Linens", "379 Lakeview Ave", "Random City6", "CA", "91978"),
+    ("Amanda", "Lewis", "Amanda@test.com", "987654", "Amandas Pest Control", "www.amandaspestcontrol.com", "Pest Control", "756 Iris Dr", "Random City7", "CA", "92154"),
+    ("Tom", "Brady", "tom@test.com", "654789", "Toms Window Repair", "www.tomswindowrepair.com", "Window Repair", "899 Date Ave", "Random City8", "CA", "92023"),
+    ("Bill", "Jones", "bill@test.com", "456789", "Bills Apparel", "www.billsapparel.com", "Apparel", "472 Harbor Dr", "Random City9", "CA", "92106");
+
+    
+    
