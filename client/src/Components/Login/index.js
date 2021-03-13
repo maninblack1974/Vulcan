c9d535fa0975ccf82a28cc525505483734338e0c
Dennis Herris <25594179+maninblack1974@users.noreply.github.com>
2021-03-13 11:29:45 -0800

    Revert "Merge b"

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,88 +1,33 @@
-import { React } from 'react';
+import React, { Component } from "react";
 
-import { GoogleLogin } from 'react-google-login';
-// refresh token
-import { refreshTokenSetup } from '../../Utils/refreshToken';
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
 
-const clientId =
-  '827360591703-tgm50hh32gmsb3af5l2fi5kl8bd0v1j0.apps.googleusercontent.com';
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
 
-// var isSignedIn = false;
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-function Login() {
-  const onSuccess = (res) => {
-    console.log('Login Success: currentUser:', res.profileObj);
-    // alert(
-    //   `Logged in successfully welcome ${res.profileObj.name}`
-    // );
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
 
-    refreshTokenSetup(res);
-
-  };
-
-  const onFailure = (res) => {
-    console.log('Login failed: res:', res);
-    alert(
-      `Failed to login.`
-    );
-  };
-
-
- 
-    return (
-      <div>
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Login"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          style={{ marginTop: '100px' }}
-          isSignedIn={true}
-        />
-      </div>
-    );
-} 
-
-
-  
-
-
-export default Login;
-
-
-
-
-// import React, { Component } from "react";
-
-// export default class Login extends Component {
-//     render() {
-//         return (
-//             <form>
-//                 <h3>Sign In</h3>
-
-//                 <div className="form-group">
-//                     <label>Email address</label>
-//                     <input type="email" className="form-control" placeholder="Enter email" />
-//                 </div>
-
-//                 <div className="form-group">
-//                     <label>Password</label>
-//                     <input type="password" className="form-control" placeholder="Enter password" />
-//                 </div>
-
-//                 <div className="form-group">
-//                     <div className="custom-control custom-checkbox">
-//                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
-//                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-//                     </div>
-//                 </div>
-
-//                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
-//                 <p className="forgot-password text-right">
-//                     Forgot <a href="#">password?</a>
-//                 </p>
-//             </form>
-//         );
-//     }
-// }
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+        );
+    }
+}
--- client/src/Components/SignUp/index.js
+++ client/src/Components/SignUp/index.js
@@ -1,43 +1,42 @@
+import React, { Component } from "react";
 
-// import React, { Component } from "react";
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+            <form>
+                <h3>Sign Up</h3>
 
-// export default class SignUp extends Component {
-//     render() {
-//         return (
-//             <div>
-//             <form>
-//                 <h3>Sign Up</h3>
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
 
-//                 <div className="form-group">
-//                     <label>First name</label>
-//                     <input type="text" className="form-control" placeholder="First name" />
-//                 </div>
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
 
-//                 <div className="form-group">
-//                     <label>Last name</label>
-//                     <input type="text" className="form-control" placeholder="Last name" />
-//                 </div>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
 
-//                 <div className="form-group">
-//                     <label>Email address</label>
-//                     <input type="email" className="form-control" placeholder="Enter email" />
-//                 </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-//                 <div className="form-group">
-//                     <label>Password</label>
-//                     <input type="password" className="form-control" placeholder="Enter password" />
-//                 </div>
-
-//                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-//                 <p className="forgot-password text-right">
-//                     Already registered <a href="#">sign in?</a>
-//                 </p>
-//             </form>
-//             <p>
-//                 OR
-//             </p>
-//             <div className="g-signin2" data-onsuccess="onSignIn">Sign in using Google, yo!</div>
-//         </div>
-//         );
-//     }
-// }
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+            <p>
+                OR
+            </p>
+            <div className="g-signin2" data-onsuccess="onSignIn">Sign in using Google, yo!</div>
+        </div>
+        );
+    }
+}
