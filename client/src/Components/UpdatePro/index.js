db659e201ad42276c3d98f876749d18588018385
pmo48 <pmoriarty100@gmail.com>
2021-03-23 16:30:30 -0700

    changed update pro logic

--- client/src/Components/UpdatePro/index.js
+++ client/src/Components/UpdatePro/index.js
@@ -1,8 +1,102 @@
 import React, { Component } from "react";
 import axios from "axios";
-import "./style.css";
+// import "./style.css";
+import styled from 'styled-components';
 
+const ProInfo = styled.div`
+  margin: 0 auto 0 auto;
+  @media (min-width: 482px) {
+    max-width: 449px;
+  }
 
+  @media (min-width: 701px) {
+    max-width: 668px;
+  }
+
+  @media (min-width: 1026px) {
+    max-width: 946px;
+  }
+  > h1 {
+    color: #7D7B6D;
+    font-size: 24px;
+    line-height: 28px;
+    font-weight: 700;
+    text-align: center;
+    margin-top: 34px;
+    margin-bottom: 24px;
+    @media (max-width: 701px) {
+      margin-top: 100px;
+    }
+  }
+  > form {
+    background-color: #7D7B6D;
+    border: 1px solid #e9eced;
+    border-radius: 4px;
+    padding: 32px 72px 40px;
+    margin-bottom: 40px;
+    margin: auto;
+    @media (max-width: 701px) {
+      border: none;
+    }
+    > input {
+      width: 100%;
+      background-color: #fff;
+      border: 1px solid #d3d4d5;
+      border-radius: 4px;
+      padding: 13px 15px;
+      font-size: 16px;
+      vertical-align: middle;
+      line-height: 24px;
+      margin: 0 0 5px;
+    }
+    > label {
+      color: #FFE033;
+      display: block;
+      margin: 4px 0 4px 0;
+      font-weight: 700;
+    }
+    > p {
+      text-align: center;
+      margin-top: 16px;
+      font-size: 12px;
+      line-height: 18px;
+    }
+    > button {
+      width: 100%;
+      text-align: center;
+      box-sizing: border-box;
+      display: inline-block;
+      vertical-align: middle;
+      white-space: nowrap;
+      cursor: pointer;
+      margin: 0;
+      line-height: 24px;
+      font-size: 16px;
+      font-weight: 700;
+      margin: 10px 0 0 0;
+      user-select: none;
+      border-radius: 4px;
+      padding: 12px 22px;
+      overflow: visible;
+      background-color: #FFE033;
+      border: 2px solid transparent;
+      color: #7D7B6D;
+    }
+    > div {
+      display: flex;
+      align-items: center;
+      font-weight: 400;
+      color: #676d73;
+      margin-top: 16px;
+      margin-bottom: 16px;
+    }
+    a {
+      color: #009fd9;
+      fill: #009fd9;
+      cursor: pointer;
+    }
+  }
+`;
 export default class UpdatePro extends Component {
 
 
@@ -76,6 +170,24 @@ export default class UpdatePro extends Component {
 
   }
 
+  openWidget = () => {
+    // create the widget
+    cloudinary.createUploadWidget(
+      {
+        cloudName: 'august-innovations-inc',
+        uploadPreset: 'uz3n96ci',
+      },
+      (error, result) => {
+        if (result.event === 'success') {
+        this.setState({
+          imageUrl: result.info.secure_url,
+          imageAlt: `An image of ${result.info.original_filename}`
+        })}
+      },
+    ).open();
+     // open up the widget after creation
+  };
+
   handleFormSubmit(event) {
     event.preventDefault();
     const {
@@ -88,6 +200,7 @@ export default class UpdatePro extends Component {
       servicePro_state,
       servicePro_zipCode,
       servicePro_profileImg,
+      imageUrl,
 
     } = this.state;
     const formData = {
@@ -99,7 +212,7 @@ export default class UpdatePro extends Component {
       "servicePro_city": servicePro_city,
       "servicePro_state": servicePro_state,
       "servicePro_zipCode": servicePro_zipCode,
-      "servicePro_profileImg": servicePro_profileImg,
+      "servicePro_profileImg": imageUrl,
       "UserId": this.state.UserId
 
     };
@@ -139,74 +252,78 @@ export default class UpdatePro extends Component {
 
   render() {
     return (
+      <ProInfo>
+      <h1>Update Your Service</h1>
       <form className="registration-form" onSubmit={this.handleFormSubmit}>
-        <fieldset
+        {/* <fieldset
           className="form-fields container"
           disabled={this.disableForm()}
-        >
-          <ul className="form-container col-lg-6" >
-          {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+        > */}
+          {/* <ul className="form-container col-lg-6" > */}
+            {/* <li className="form-list-item"> */}
+            {/* {this.state.existingProfile.map(item => ( */}
               <label htmlFor="servicePro_companyName">COMPANY NAME</label>
               <input
                 type="text"
                 id="servicePro_companyName"
                 name="servicePro_companyName"
                 placeholder="Company Name"
-                defaultValue={item.servicePro_companyName}
+                // defaultValue={item.servicePro_companyName}
                 onChange={this.handleInputChange}
               />
-            </li>
-            ))}
-          {this.state.existingProfile.map(item => (
+              {/* ))} */}
+            {/* </li> */}
+           
+          {/* {this.state.existingProfile.map(item => ( */}
 
-            <li className="form-list-item">
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_url">WEBSITE</label>
               <input
                 type="text"
                 id="servicePro_url"
                 name="servicePro_url"
                 placeholder="Website"
-                defaultValue={item.servicePro_url}
+                // defaultValue={item.servicePro_url}
                 onChange={this.handleInputChange}
               />
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-              <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+              {/* <li className="form-list-item"> */}
                 <label htmlFor="servicePro_phone">PHONE NUMBER</label>
                 <input
                   type="text"
                   id="servicePro_phone"
                   name="servicePro_phone"
                   placeholder="Phone Number"
-                  defaultValue={item.servicePro_phone}
+                  // defaultValue={item.servicePro_phone}
                   onChange={this.handleInputChange}
                 />
-              </li>
-            ))}
-            {this.state.existingProfile.map(item => (
+              {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
 
-            <li className="form-list-item">
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_profileImg">PROFILE IMAGE</label>
               <input
-                type="file"
+                type="button"
+                className="btn widget-btn"
                 id="servicePro_profileImg"
                 name="servicePro_profileImg"
                 placeholder="Image or Logo"
-                defaultValue={item.servicePro_profileImg}
-                onChange={this.handleFileChange}
+                // defaultValue={item.servicePro_profileImg}
+                onClick={this.openWidget}
               />
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_category">CATEGORY</label>
               <select
                 type="text"
                 id="servicePro_category"
                 name="ServicePro_category"
-                defaultValue={item.servicePro_category}
+                // defaultValue={item.servicePro_category}
                 onChange={this.handleInputChange}
               >
                 <option defaultValue="">Choose...</option>
@@ -226,42 +343,42 @@ export default class UpdatePro extends Component {
                 <option value="Window Repair">Window Repair</option>
                 <option value="Table Booth Repair">Table Booth Repair</option>
               </select>
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_address">ADDRESS</label>
               <input
                 type="text"
                 id="servicePro_address"
                 name="servicePro_address"
                 placeholder="Address"
-                defaultValue={item.servicePro_address}
+                // defaultValue={item.servicePro_address}
                 onChange={this.handleInputChange}
               />
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_city">City</label>
               <input
                 type="text"
                 id="servicePro_city"
                 name="servicePro_city"
                 placeholder="City"
-                defaultValue={item.servicePro_city}
+                // defaultValue={item.servicePro_city}
                 onChange={this.handleInputChange}
               />
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+            {/* <li className="form-list-item"> */}
               <label htmlFor="servicePro_state">STATE</label>
               <select
                 type="text"
                 id="servicePro_state"
                 name="ServicePro_state"
-                defaultValue={item.servicePro_state}
+                // defaultValue={item.servicePro_state}
                 onChange={this.handleInputChange}
               >
                 <option defaultValue="">Choose...</option>
@@ -317,10 +434,10 @@ export default class UpdatePro extends Component {
                 <option value="WI">Wisconsin</option>
                 <option value="WY">Wyoming</option>
               </select>
-            </li>
-            ))}
-            {this.state.existingProfile.map(item => (
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* {this.state.existingProfile.map(item => ( */}
+            {/* <li className="form-list-item"> */}
               <label className="form-zip-label" htmlFor="servicePro_zipCode">
                 ZIP
                   </label>
@@ -329,22 +446,23 @@ export default class UpdatePro extends Component {
                 id="servicePro_zipCode"
                 name="servicePro_zipCode"
                 placeholder="Zip"
-                defaultValue={item.servicePro_zipCode}
+                // defaultValue={item.servicePro_zipCode}
                 onChange={this.handleInputChange}
               />
-            </li>
-            ))}
-            <li className="form-list-item">
+            {/* </li> */}
+            {/* ))} */}
+            {/* <li className="form-list-item"> */}
               <input
                 className="form-submit-btn"
                 type="submit"
                 value="Submit"
               />
               {this.renderSuccessMessage()}
-            </li>
-          </ul>
-        </fieldset>
+            {/* </li> */}
+          {/* </ul>
+        </fieldset> */}
       </form>
+      </ProInfo>
 
     )
   }
